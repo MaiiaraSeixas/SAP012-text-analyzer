e534b0e4108dd6e0a64df127b0d872e0ded08094
Maiara Seixas <maiarasmendes21@gmail.com>
2024-01-18 20:32:21 -0300

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import  analyzer  from './analyzer.js';
-// Espera que todo o conteúdo da página seja carregado antes de executar o script
+// Espera que todo o conteúdo da página seja carregado puxando conteudos do html 
 
 document.addEventListener('input', function () {
   const entradaTexto = document.querySelector('textarea[name="user-input"]');
@@ -12,12 +12,12 @@ document.addEventListener('input', function () {
   const botaoReset = document.getElementById('reset-button');
 
 
-  // Esta adicionando um evento que é acionado a cada tecla pressionada
+  // Esta adicionando um evento de escuta para cada vez que add uma tecla
 
   entradaTexto.addEventListener('input', function () {
     const texto = entradaTexto.value;
 
-    // Está atualizando a pagina de acordo com os elementos do site e mostrando os resultados das análises que contem no analayzer
+  // Está atualizando a pagina de acordo com os elementos do site e mostrando os resultados das análises que contem no analayzer
     contagemPalavras.textContent = 'Contagem de Palavras: ' + analyzer.getWordCount(texto);
     contagemCaracteres.textContent = 'Contagem de Caracteres: ' + analyzer.getCharacterCount(texto);
     contagemCaracteresSemEspaco.textContent = 'Caracteres sem Espaços e Pontuação: ' + analyzer.getCharacterCountExcludingSpaces(texto);
@@ -28,12 +28,12 @@ document.addEventListener('input', function () {
   //Estou determinado que o botão de resetar, zere as funções declarando cada uma delas para zerar
   botaoReset.addEventListener('click', function () {
     entradaTexto.value = '';
-    contagemPalavras.textContent = 'Contagem de Palavras: 0';
-    contagemCaracteres.textContent = 'Contagem de Caracteres: 0';
-    contagemCaracteresSemEspaco.textContent = 'Caracteres sem Espaços e Pontuação: 0';
-    contagemNumeros.textContent = 'Contagem de Números: 0';
-    somaNumeros.textContent = 'Soma Total dos Números: 0';
-    comprimentoMedioPalavras.textContent = 'Comprimento Médio das Palavras: 0';
+    contagemPalavras.textContent = 'Contagem de Palavras: ';
+    contagemCaracteres.textContent = 'Contagem de Caracteres: ';
+    contagemCaracteresSemEspaco.textContent = 'Caracteres sem Espaços e Pontuação: ';
+    contagemNumeros.textContent = 'Contagem de Números: ';
+    somaNumeros.textContent = 'Soma Total dos Números: ';
+    comprimentoMedioPalavras.textContent = 'Comprimento Médio das Palavras: ';
   });
 });
 
